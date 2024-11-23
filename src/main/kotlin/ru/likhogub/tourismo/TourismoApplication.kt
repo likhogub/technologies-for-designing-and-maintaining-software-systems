9085b75f271532e6eefe331cf26fda8bdb24e82e
Oleg Likhogub <oligli@yandex.ru>
2024-11-23 23:20:58 +0300

    Настроен базоый URL SwaggerUI

--- src/main/kotlin/ru/likhogub/tourismo/TourismoApplication.kt
+++ src/main/kotlin/ru/likhogub/tourismo/TourismoApplication.kt
@@ -1,9 +1,16 @@
 package ru.likhogub.tourismo
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	servers = [
+		Server(url = "/")
+	]
+)
 class TourismoApplication
 
 fun main(args: Array<String>) {
